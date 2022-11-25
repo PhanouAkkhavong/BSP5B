9911634800a2408cb7ad3a1b7927dade5a86f6ea
PhanouAkkhavong <113885124+PhanouAkkhavong@users.noreply.github.com>
2022-11-25 21:00:16 +0700

    Add files via upload

---
+++ j.js
@@ -0,0 +1,70 @@
+var attempt = 3; 
+function loginhub(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+//admin
+if ( username == "admin" && password == "123456789"){
+alert ("Login successfully");
+window.location = "Homework2.html"; // Redirecting to other page.
+return false;
+}
+else{
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("loginsubmit").disabled = true;
+return false;
+}
+}
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+
+//user
+if ( username == "user" && password == "123456789"){
+alert ("Login successfully");
+window.location = "client.html"; // Redirecting to other page.
+return false;
+}
+else{
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("loginsubmit").disabled = true;
+return false;
+}
+}
+
+
+//error
+if ( username == "" && password == ""){
+    window.location = "e.html"; // Redirecting to other page.
+    return false;
+    }
+    else{
+    if( attempt == 0){
+    document.getElementById("username").disabled = false;
+    document.getElementById("password").disabled = false;
+    document.getElementById("loginsubmit").disabled = false;
+    return false;
+    }
+    }
+   
+    
+//incorrect 
+if ( username == "" && password == ""){
+    return false;
+    }
+    else{
+    alert("Your username or password is incorrect");
+    if( attempt == 0){
+    document.getElementById("username").disabled = true;
+    document.getElementById("password").disabled = false;
+    document.getElementById("loginsubmit").disabled = false;
+    return false;
+    }
+    }
+        
+
+}
